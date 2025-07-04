d2310a38376bd1a80d16266c001957f18b5b9423
akshatmikki <akshat.singhal@thynkweb.com>
2025-07-04 12:10:32 +0530

    changes

--- src/app/ProductsPage/page.tsx
+++ src/app/ProductsPage/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import Link from "next/link";
 import { NavBar } from "@/components/NavBar";
 import { WavyBackground } from "@/components/ui/Wavy-background";
