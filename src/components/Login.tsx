54cdd5a09f0fbebf888b80e2eb693591bb8098f7
Akshat Singhal <71835769+akshatmikki@users.noreply.github.com>
2026-01-20 10:32:49 +0530

    Fix import path for NavBar component

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { Menu, MenuItem } from "./ui/Navbar";
+import { Menu, MenuItem } from "./ui/NavBar";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
